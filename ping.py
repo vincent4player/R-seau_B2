b0855c5e9c3977947685573d31cbfcbe66bf6a84
badusch <vincent.badusch@gmail.com>
2024-10-18 10:37:36 +0200

    tp3

--- ping.py
+++ ping.py
@@ -4,7 +4,7 @@ ping = ICMP(type=8)
 
 packet = IP(src="10.33.73.77", dst="10.33.73.72")
 
-frame = Ether(src="98-8D-46-C4-FA-E5", dst="30:89:4a:d2:5a")
+frame = Ether(src="98:8D:46:C4:FA:E5", dst="30:89:4a:d2:5a")
 
 final_frame = frame/packet/ping
 
