4fe526a8c955b9c849288dff5b88889ce7ee3c8d
badusch <vincent.badusch@gmail.com>
2024-10-18 10:41:34 +0200

    tp3

--- ping.py
+++ ping.py
@@ -2,9 +2,9 @@ from scapy.all import *
 
 ping = ICMP(type=8)
 
-packet = IP(src="10.33.73.77", dst="10.33.73.72")
+packet = IP(src="10.33.73.77", dst="10.33.67.174")
 
-frame = Ether(src="98:8D:46:C4:FA:E5", dst="30:89:4a:d2:5a")
+frame = Ether(src="98:8D:46:C4:FA:E5", dst="f2:39:c5:c0:07:e5")
 
 final_frame = frame/packet/ping
 
